f43794ffd811d7cdd2b499c6def2909018b1a226
desktop <luis@4suredesign.com>
2016-05-30 21:32:39 -0500

    Programa de motor con decicion decision de direccion a partir de velocidad , y la velocidad decidida por medio de PWM con un programa en C

---
+++ codigopi/Motor_encoder/motor/fisher_rojo.py
@@ -0,0 +1,30 @@
+import pigpio
+
+class Motor_encoder(object):
+    """docstring for Motor_encoder"""
+    def __init__(self, pinA , pinB):
+        self.pinA = pinA
+        self.pinB = pinB
+        self.pi = pigpio.pi()
+
+    def avance(self , velocidad):
+        if velocidad > 0:    # si la velocidad es mayor a cero definimos pin hacia adelante , para que ese pin actue el PWM y apagamos el PWM en el otro pin 
+            pin = self.pinA
+            self.pi.set_PWM_cycle(self.pinB , 0)
+        elif velocidad < 0: #Si la velocidad es menor a cero , el motor debera ir hacia atras 
+            pin = self.pinB
+            velocidad = abs(velocidad)
+            self.pi.set_PWM_cycle(self.pinA , 0)
+
+        if not velocidad:    
+            self.pi.write(self.pinA , 0) #Si la velocidad es 0 paramos el motor mandando 0 a los 2 pines del L293D
+            self.pi.write(self.pinB , 0)    
+        
+        if abs(velocidad) > 255 : velocidad = 255
+        
+        self.pi.set_PWM_cycle(pin , velocidad) # Rodar motor a la direccion decidida , a la velocidad propuesta
+
+    def stop(self):
+        self.pi.set_PWM_cycle(self.pinA , 0)
+        self.pi.set_PWM_cycle(self.pinB , 0)        
+
